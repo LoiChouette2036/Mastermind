8a5612849dad8599b6881112e404ed1936cffa8d
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-08-01 16:22:40 +0900

    in the  board file, board can be seen and initialize

--- board.rb
+++ board.rb
@@ -1,4 +1,16 @@
 require 'colorize'
 
-class board
+class Board
+    def initialize
+        @board_2d = Array.new(12) { Array.new(4)}
+
+        #print the 2D array to see its structure
+        @board_2d.each_with_index do |inner_array, index|
+            puts "Array #{index+1}: #{inner_array.inspect}"
+        end
+    end
+
+
 end
+
+board = Board.new
