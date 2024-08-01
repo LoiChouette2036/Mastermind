e08afd0f17164e12ac5a09ba178aed5154a4ad16
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-08-01 16:53:08 +0900

    updated board.rb, display two array

--- board.rb
+++ board.rb
@@ -3,10 +3,11 @@ require 'colorize'
 class Board
     def initialize
         @board_2d = Array.new(12) { Array.new(4)}
+        @board_help = Array.new(12) {Array.new(4)}
 
         #print the 2D array to see its structure
         @board_2d.each_with_index do |inner_array, index|
-            puts "Array #{index+1}: #{inner_array.inspect}"
+            puts "Try #{index+1}: #{inner_array.inspect}    #{@board_help[index]}"
         end
     end
 
